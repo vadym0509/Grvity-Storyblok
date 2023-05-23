e42f8f2711171e8b07919b7934c093bc81278f5a
vadymrass <vadymrass0509@gmail.com>
2023-05-23 07:13:53 +0200

    fixed one error

--- components/section-with-screenshot/section-with-screenshot.js
+++ components/section-with-screenshot/section-with-screenshot.js
@@ -8,7 +8,8 @@ function getSectionWithScreenshot(block){
             content += `    </div>\
                         </div>\
                         <div class="flex">
-                            <img class="w-full drop-shadow-xl md:ml-7 mt-6 md:mt-0 md:absolute md:-right-8 top-60 lg:top-auto xl:top-56 2xl:top-auto min-[1921px]:right-auto md:w-[400px] md:h-[262px] lg:w-[520px] lg:h-[340px] xl:w-[600px] xl:h-[393px] 2xl:w-[720px] 2xl:h-[472px] min-[1920px]:w-[905px] min-[1920px]:h-[593px]" src=${block.screenshot.filename} alt="screenshot_1_large">\
+                            <img class="w-full hidden md:block drop-shadow-xl md:ml-7 md:mt-0 md:absolute right-0 top-60 lg:top-auto xl:top-56 2xl:top-auto min-[1921px]:right-auto md:w-[400px] md:h-[262px] lg:w-[520px] lg:h-[340px] xl:w-[600px] xl:h-[393px] 2xl:w-[720px] 2xl:h-[472px] min-[1920px]:w-[905px] min-[1920px]:h-[593px]" src=${block.screenshot_large.filename} alt="screenshot_1_large">\
+                            <img class="w-full mt-6 md:hidden drop-shadow-xl" src=${block.screenshot_small.filename} alt="screenshot_1_small">
                         </div>
                     </div>`
     return content;
