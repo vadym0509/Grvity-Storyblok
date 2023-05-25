0f9b8d77914ce8be95c63b848b0fc7fc17355bd2
vadymrass <vadymrass0509@gmail.com>
2023-05-25 10:52:14 +0200

    added the mobile navigation

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,10 +1,10 @@
 function getFooter(body){
-    var content = `<div id="footer-container" class="w-full flex justify-center bg-[#F6F9FC]">
+    var content = `<div id="footer-container" class="w-full flex mt-14 sm:mt-18 justify-center bg-[#F6F9FC]">
                         <div class="my-24 mx-8 w-full md:w-[719px] lg:w-[960px] xl:w-[1048px]">
                             <div class="sm:inline-flex w-full">
                                 <div class="mr-10 md:mr-16 lg:mr-24 w-full sm:w-[126px] md:w-[158px] lg:w-auto">
                                     <img class="mt-3" src="${body.footer_logo.filename}" alt="${body.footer_logo.alt}">
-                                    <div class="sm:hidden sm:block text-base text-[#797979] mt-4">${new Date().getFullYear()}&nbsp;&copy;&nbsp;GRVITY</div>
+                                    <div class="sm:hidden sm:block text-base text-[#797979] mt-4 mb-6">${new Date().getFullYear()}&nbsp;&copy;&nbsp;GRVITY</div>
                                 </div>
                                 <div class="inline-flex w-full sm:w-3/4">
                                     <div class="w-1/2 sm:w-1/3 mr-14 sm:mr-0">`
@@ -25,7 +25,7 @@ function getFooter(body){
 }
 
 function getFooterItems(block){
-    var content = `<div class="sm:w-1/2"><ul><li class="font-semibold text-2xl text-[#323B4B] mb-9">${block.title}</li>`
+    var content = `<div class="sm:w-1/2"><ul><li class="font-semibold text-2xl text-[#323B4B] mb-4 sm:mb-9">${block.title}</li>`
     block.items.map(function(item, index){
         content += `<li class="text-[#4E5D78] text-lg leading-[28px]"><a href="${item.link.url}">${item.text}</a></li>`
     })
---
+++ components/mobile-navigation/mobile-navigation.js
@@ -0,0 +1,29 @@
+function getMobileNavigation(body) {
+    var content = `<div id="mobile-nav" class="absolute hidden lg:hidden top-4 right-5 p-4 w-[328px] drop-shadow-2xl rounded-[10px] bg-white z-[1]">
+                        <div class="flex justify-between w-full py-3">
+                            <div class="w-[165px] h-4"><img src="${body.logo.filename}" alt="${body.logo.alt}"></div>
+                            <button onclick="handleMobileNavBtnClick()" class="w-4 h-4"><img src="${body.close_icon.filename}" alt="${body.close_icon.alt}"></button>
+                        </div>
+                        <div class="flex flex-col mt-4">`;
+            body.items.map(function(item, index){
+                content += `<div id="mobile-nav-accordion-item-${index}" ${item.links.length > 0 ? `onclick="document.getElementById('mobile-nav-links-container-${index}').classList.toggle('hidden');document.getElementById('mobile-nav-accordion-item-${index}').classList.toggle('bg-[#F6F9FC]');document.getElementById('mobile-nav-accordion-down-icon-${index}').classList.toggle('hidden');document.getElementById('mobile-nav-accordion-up-icon-${index}').classList.toggle('hidden');"` : ``}  class="w-full px-4 pt-4 flex justify-between cursor-pointer rounded-t-[10px]">
+                                <div class="w-full font-semibold text-lg text-[#4E5D78]">${item.label}</div>
+                                ${item.links.length > 0 ?
+                                `<button class="" id="mobile-nav-accordion-down-icon-${index}"><img src="${item.accordion_icon_down.filename}" alt="${item.accordion_icon_down.alt}"></button>
+                                <button class="hidden" id="mobile-nav-accordion-up-icon-${index}"><img src="${item.accordion_icon_up.filename}" alt="${item.accordion_icon_up.alt}"></button>` : ""}
+                            </div>`;
+                if(item.links.length > 0){
+                    content += `<div id="mobile-nav-links-container-${index}" class="hidden flex flex-col p-4 gap-y-4 bg-[#F6F9FC] rounded-b-[10px]"><hr>`;
+                    item.links.map(function(link, index){
+                        content += `<div>
+                                        <div class="font-semibold text-lg text-[#323B4B]">${link.text}</div>
+                                        <div class="text-sm text-[#8A94A6]">${link.description}</div>
+                                    </div>`;
+                    })
+                    content += `</div>`;
+                }
+            })                   
+            content += `</div>
+                    </div>`;
+    return content;
+}
