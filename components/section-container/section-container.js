fb6b6e12e06896b82556d3b6c089d1db99313466
vadymrass <vadymrass0509@gmail.com>
2023-05-29 14:38:13 +0200

    fixed the section indicators issue

--- components/section-container/section-container.js
+++ components/section-container/section-container.js
@@ -1,16 +1,17 @@
 function getSectionContainer(body){
     document.addEventListener("scroll", function(e){
+        console.log(window.scrollY)
         if(window.scrollY > 820){
             document.getElementById("sticky-header-nav").style.transform = "translateY(0px)";
         }else{
             document.getElementById("sticky-header-nav").style.transform = "translateY(-90px)";
         }
-        setScrollSpeed("01", 0);
-        setScrollSpeed("02", 540);
+        setScrollSpeed("01", -150);
+        setScrollSpeed("02", 400);
         setScrollSpeed("03", 1600);
-        setScrollSpeed("04", 2300);
+        setScrollSpeed("04", 2200);
         setScrollSpeed("05", 3400);
-        setScrollSpeed("06", 4000);
+        setScrollSpeed("06", 3900);
     })
     var content = '<div id="section-container" class="w-full mx-8 md:w-[719px] lg:w-[960px] xl:w-[1048px]">';
     content += getHeaderNav(body[0]);
