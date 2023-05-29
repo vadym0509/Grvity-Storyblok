2a4c87d0767243c16fc70851203b3842e767faf8
vadymrass <vadymrass0509@gmail.com>
2023-05-29 10:29:19 +0200

    added the vertical watermarks to the home page

---
+++ components/vertical-watermarks/vertical-watermarks.js
@@ -0,0 +1,8 @@
+function getVerticalWatermarks(){
+    var content = `<div class="absolute z-[1] font-bold text-[150px] md:text-[100px] lg:text-[150px] 2xl:text-[250px] text-[#5b7fff33] mt-[1500px] lg:mt-[1340px] left-[20px] lg:-ml-[1345px] lg:left-auto">
+                        <div class="rotate-90 lg:-rotate-90">Understand</div>
+                        <div class="mt-[1560px] lg:mt-[1140px] rotate-90 lg:-rotate-90">Engage</div>
+                        <div class="mt-[1100px] lg:mt-[1600px] rotate-90 lg:-rotate-90">Incentivise</div>
+                    </div>`;
+    return content;
+}
