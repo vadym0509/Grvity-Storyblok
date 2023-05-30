9de33d5d12792bb3de752991bde056ff11d2e543
vadymrass <vadymrass0509@gmail.com>
2023-05-30 14:06:23 +0200

    fixed hero image issue in the header section of home page

--- components/section-with-screenshot/section-with-screenshot.js
+++ components/section-with-screenshot/section-with-screenshot.js
@@ -8,7 +8,8 @@ function getSectionWithScreenshot(block){
             content += `    </div>\
                         </div>\
                         <div class="flex">
-                            <img class="w-full hidden md:block drop-shadow-xl md:ml-7 md:mt-0 md:absolute right-0 top-60 lg:top-auto xl:top-56 2xl:top-auto min-[1921px]:right-auto md:w-[400px] md:h-[262px] lg:w-[520px] lg:h-[340px] xl:w-[600px] xl:h-[393px] 2xl:w-[720px] 2xl:h-[472px] min-[1920px]:w-[905px] min-[1920px]:h-[593px]" src=${block.screenshot_large.filename} alt="screenshot_1_large">\
+                            <img class="w-full hidden md:block drop-shadow-xl md:ml-7 md:mt-0 md:absolute right-0 top-60 lg:top-auto xl:top-56 2xl:top-auto md:w-[400px] md:h-[262px] lg:w-[520px] lg:h-[340px] xl:w-[600px] xl:h-[393px] 2xl:w-[720px] 2xl:h-[472px] min-[1920px]:hidden" src=${block.screenshot_large.filename} alt="screenshot_1_large">
+                            <img class="hidden min-[1920px]:block drop-shadow-xl md:absolute right-auto w-[905px] h-[593px]" src=${block.screenshot_small.filename} alt="screenshot_1_large">\
                             <img class="w-full mt-6 md:hidden drop-shadow-xl" src=${block.screenshot_small.filename} alt="screenshot_1_small">
                         </div>
                     </div>`
