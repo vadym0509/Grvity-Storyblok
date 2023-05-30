a0e5c66d236651f1a06d12da1fa4b94846243096
vadymrass <vadymrass0509@gmail.com>
2023-05-30 14:14:07 +0200

    fixed the color of primary button

--- components/button/button.js
+++ components/button/button.js
@@ -1,5 +1,5 @@
 function getButton(block){
-    var content = `<button class="px-6 py-2 font-bold text-white rounded-full bg-blue-500 hover:bg-opacity-80">\
+    var content = `<button class="px-6 py-2 font-bold text-white rounded-full bg-[#5B7FFF] hover:bg-opacity-80">\
                         ${block.label}\
                     </button>\
                     `;
