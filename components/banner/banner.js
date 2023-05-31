86886db5360c7acc46f100d6c682b8f988b1e47c
vadymrass <vadymrass0509@gmail.com>
2023-05-31 08:44:45 +0200

    updated the responsive feature of the banner of loyalty page

--- components/banner/banner.js
+++ components/banner/banner.js
@@ -1,6 +1,6 @@
 function getBanner(block){
     var content = `<div class="flex justify-center w-full mt-24">
-                        <div class="w-[36vw]">
+                        <div class="w-full lg:w-[43.25rem]">
                             <div class="font-bold text-[#242331] text-[3.5rem] text-center leading-[4.25rem]">${block.blocks[0].headline}</div>
                             <div class="mt-6 text-[3.5rem] text-[#797979] text-center text-lg">${block.blocks[1].text.content[0].content[0].text}</div>
                         </div>
