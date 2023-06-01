64e2565d51c5267659a41f4ffa58c33a9649a773
vadymrass <vadymrass0509@gmail.com>
2023-06-01 08:02:36 +0200

    fixed the background issue in the loyalty page

--- components/section-container/section-container.js
+++ components/section-container/section-container.js
@@ -27,14 +27,15 @@ function getHomeSections(body){
 }
 
 function getLoyaltySections(body){
-    var content = `<div class="absolute overflow-hidden inset-0 -z-[1]">\
-                        <div class="h-[55rem] bg-cover w-screen bg-[url('${body[3].background_image.filename}')]"></div>\
+    var content = `<div class="absolute overflow-hidden inset-0 z-[-1]">\
+                        <div class="absolute h-[60rem] bg-no-repeat w-screen bg-[url('${body[3].background_image_1.filename}')]"></div>
+                        <div class="absolute h-[60rem] bg-no-repeat w-screen bg-[url('${body[3].background_image_2.filename}')]"></div>
                     </div>`;
     content += getHeaderNav(body[0]);
     content += getStickyHeaderNav(body[0]);
     content += getDesktopMenuContent(body[1].items[0]);
     content += getBanner(body[3])
-    content += getSectionWithoutLabels(body[4], false);
+    content += getSectionWithoutLabels(body[4], true);
     content += getSectionWithoutLabels(body[5], false);
     content += getSectionWithoutLabels(body[6], false);
     content += getTabsContainer(body[7]);
