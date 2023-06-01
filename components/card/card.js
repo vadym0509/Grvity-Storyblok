c3b4a3f65d4a7bcb144fb7cd0df8f8c52b8056a2
vadymrass <vadymrass0509@gmail.com>
2023-06-01 16:25:47 +0200

    updated the shadow effect in the modules page

--- components/card/card.js
+++ components/card/card.js
@@ -1,5 +1,5 @@
 function getCard(block){
-    var content = `<div class="mt-12 w-full h-[24rem] md:h-[19.125rem] py-[1rem] px-6 rounded-[20px] bg-white" style="box-shadow: 4px 4px 40px rgba(138, 148, 166, 0.25);">
+    var content = `<div class="mt-12 w-full h-[25rem] md:h-[19.125rem] py-[1rem] px-6 rounded-[20px] bg-white" style="box-shadow: 4px 4px 40px rgba(138, 148, 166, 0.25);">
                         <div class="inline-flex">
                             <div class="mr-6 font-bold text-[1.625rem] text-[#323B4B]">${block.title}</div>
                             <div class="m-auto">`
@@ -14,7 +14,7 @@ function getCard(block){
                                 <div class="text-[1rem] leading-7 text-[#323B4B]">${item.text.content[0].content[0].text}</div>
                             </div>`
             })
-            if(block.detail_url.url) content += `<a href="${block.detail_url.url}" class="relative -bottom-[1rem] left-[57.8rem] font-semibold text-[0.9375rem] text-[#5B7FFF] hover:underline">See details</a>`            
+            if(block.detail_url.url) content += `<div class="mt-0 md:mt-4 flex w-full justify-end"><a href="${block.detail_url.url}" class="font-semibold text-[0.9375rem] text-[#5B7FFF] hover:underline">See details</a></div>`            
             content += `</div>`;    
         content += `</div>`;
     return content;
