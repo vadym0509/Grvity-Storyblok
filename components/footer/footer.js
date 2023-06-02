2954dc8b1c6a3c0ebc575171175a748c27bd122a
vadymrass <vadymrass0509@gmail.com>
2023-06-02 18:44:59 +0200

    fixed some style issues in the footer section

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -8,11 +8,11 @@ function getFooter(body){
                                 </div>
                                 <div class="inline-flex w-full sm:w-3/4">
                                     <div class="w-1/2 sm:w-1/3 mr-14 sm:mr-0">`
-                                        content += getFooterItems(body.blocks[0]);
+                                        content += getFooterItems(body.blocks[0], "sm:w-1/2 md:w-full");
                         content += `</div>
                                     <div class="sm:inline-flex w-1/2 sm:w-2/3">`
-                                        content += getFooterItems(body.blocks[1]);
-                                        content += '<div class="mt-6 sm:mt-0">'
+                                        content += getFooterItems(body.blocks[1], "sm:w-1/2");
+                                        content += '<div class="mt-6 sm:mt-0 sm:w-1/2">'
                                         content += getFooterItems(body.blocks[2]);
                                         content += '</div>'
                         content += `</div>
@@ -24,10 +24,10 @@ function getFooter(body){
     return content;
 }
 
-function getFooterItems(block){
-    var content = `<div class="sm:w-1/2"><ul><li class="font-semibold text-2xl text-[#323B4B] mb-4 sm:mb-9">${block.title}</li>`
+function getFooterItems(block, width){
+    var content = `<div class="${width}"><ul><li class="font-semibold text-2xl text-[#323B4B] mb-4 sm:mb-9">${block.title}</li>`
     block.items.map(function(item, index){
-        content += `<li class="text-[#4E5D78] text-lg leading-[28px]"><a href="${item.link.url}">${item.text}</a></li>`
+        content += `<li class="text-[#4E5D78] text-lg leading-[28px] hover:underline"><a href="${item.link.url}">${item.text}</a></li>`
     })
     content += '</ul></div>'
     return content;
