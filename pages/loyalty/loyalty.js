66b8dfb305977ba04d7c3045bf34d044badf8a0d
vadymrass <vadymrass0509@gmail.com>
2023-06-02 13:55:19 +0200

    fixed a style issue.

--- pages/loyalty/loyalty.js
+++ pages/loyalty/loyalty.js
@@ -1,7 +1,7 @@
 function getLoyaltySections(body){
     var content = `<div class="absolute overflow-hidden inset-0 z-[-1]">
                         <div class="absolute h-[60rem] bg-no-repeat bg-cover w-screen bg-[url('${body[3].background_image_1.filename}')]"></div>
-                        <div class="absolute h-[60rem] bg-no-repeat bg-cover w-screen bg-[url('${body[3].background_image_2.filename}')]"></div>
+                        <div class="absolute h-[60rem] bg-no-repeat w-screen bg-[url('${body[3].background_image_2.filename}')]"></div>
                     </div>`;
     content += getHeaderNav(body[0]);
     content += getStickyHeaderNav(body[0]);
